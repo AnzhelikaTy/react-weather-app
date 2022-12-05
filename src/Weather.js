8b1f1e96a572a3ec370bf2375677bc405fd123dd
Anzhelika Susemcik <angelika.tyrnova@gmail.com>
2022-12-04 23:54:05 -0500

    added axios, API and styles

--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Watch } from "react-loader-spinner";
+export default function Weather() {
+  let [city, setCity] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState(null);
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )}°C`
-    );
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.name,
+      country: response.data.sys.country,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      windSpeed: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
   }
-  let apiKey = "9d81c26a5595fda01e1226df6075f13e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <Watch
-      height="80"
-      width="80"
-      radius="48"
-      color="#00BFFF"
-      ariaLabel="watch-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "9d81c26a5595fda01e1226df6075f13e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul className="Weather">
+          <li>
+            {weather.city}, {weather.country}
+          </li>
+          <li>Temperature: {Math.round(weather.temperature)}ºC</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>WindSpeed: {Math.round(weather.windSpeed * 2.237)} mph</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
